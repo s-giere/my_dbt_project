42084970ea07ac9ba8bc94ccacdc9813be9a358e
Swetlana Giere <swetlana.giere@gmail.com>
2023-10-25 13:35:27 +0000

    Added new field for model X

--- models/marts/rpt_store_initial_efficiency.sql
+++ models/marts/rpt_store_initial_efficiency.sql
@@ -11,6 +11,7 @@ lag_timestamp_calc as (
 select
 	store_id,
 	transaction_at,
+    transaction_order,
 	lag(transaction_at) over (partition by store_id order by transaction_at) as lag_transaction_at
 from {{ ref('int_first_5_store_transactions') }}
 
@@ -20,6 +21,7 @@ final as (
 
 select
 	store_id,
+    max(transaction_order) as n_accepted_transactions,
 
 	case
 		when count(*) = 5 then true
